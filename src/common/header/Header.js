e543448644a14486010ebd995eab4496dd459e87
haneefsyedanywhere <haneef.syed@anywhere.co>
2022-01-14 11:53:24 +0530

    added login

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,21 +1,218 @@
-import React from 'react'
-import Button from '@mui/material/Button';
+import React, { useState } from 'react'
+import Button from '@material-ui/core/Button';
+import logo from '../../assets/logo.svg';
+import './Header.css';
+import PropTypes from 'prop-types';
+import Modal from 'react-modal';
+import Tab from '@material-ui/core/Tab';
+import Tabs from '@material-ui/core/Tabs';
+import Typography from '@material-ui/core/Typography';
+import AppBar from '@material-ui/core/AppBar';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import { Link } from 'react-router-dom';
+import Grid from '@material-ui/core/Grid';
+
+function TabContainer(props) {
+  return (
+    <Typography component="div" style={{ padding: 8 * 3 }}>
+      {props.children}
+    </Typography>
+  );
+}
+
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+});
+
+
+
+
 
 
 export default function Header(props) {
-    let isLoggedIn;
+
+    const [value, setValue] = useState(0);
+    const [isLoggedIn,setIsLoggedIn] = useState(false);
+    const [isRegistrationSuccessful,setIsRegistrationSuccessful] = useState(false);
+
+    const [loginFormInputs, setLoginFormInputs] = useState({
+      username : '',
+      password : ''
+    });
+    const [sigInFormInputs, setSignInFormInputs] = useState({
+      firstName : '',
+      lastName : '',
+      email : '',
+      password : '',
+      contact : ''
+    });
+
+    const customStyles = {
+        content: {
+          top: '50%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
+        },
+      };
+      const [modalIsOpen, setIsOpen] = React.useState(false);
+
+      function openModal() {
+        setIsOpen(true);
+      }
+    
+      function closeModal() {
+        setIsOpen(false);
+      }  
+
+      const loginClickHandler = ()=>{
+          openModal();
+      }
+      const handleChange = (event, value) => {
+        setValue(value);
+      };
+
+
+      const handleLoginFormInputChange = (e)=>{
+        const state = {...loginFormInputs};
+        state[e.target.name] = e.target.value;
+        setLoginFormInputs(state);
+      }
+
+      const handleSignInFormInputChange = (e)=>{
+        const state = {...sigInFormInputs};
+        state[e.target.name] = e.target.value;
+        setSignInFormInputs(state);
+      }
+
+      const loginBtnHandler = (e)=>{
+        if(loginFormInputs.username.length>0 && loginFormInputs.password.length>0){
+          console.log('Logging in!', loginFormInputs)
+        }
+      }
+
+      const signInBtnHandler = (e)=>{
+        if(sigInFormInputs.firstName.length>0 && sigInFormInputs.lastName.length>0 && sigInFormInputs.password.length>0 && sigInFormInputs.email.length>0 && sigInFormInputs.contact.length>0){
+          console.log('Signing in!', sigInFormInputs)
+        }
+      }
+
     return (
-        <header>
-            <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-        	 width="512px" height="512px" viewBox="0 0 512 512" enable-background="new 0 0 512 512" className="logo">
-                <path d="M352,255.5l-192,96v-192L352,255.5z M512,31.5v448H0v-448H512z M320,95.5h64v-32h-64V95.5z M224,95.5h64v-32h-64V95.5z
-                    M128,95.5h64v-32h-64V95.5z M32,95.5h64v-32H32V95.5z M96,415.5H32v32h64V415.5z M192,415.5h-64v32h64V415.5z M288,415.5h-64v32h64
-                    V415.5z M384,415.5h-64v32h64V415.5z M480,415.5h-64v32h64V415.5z M480,127.5H32v256h448V127.5z M480,63.5h-64v32h64V63.5z"/>
-            </svg>
+        <header className="app-header">
+            <img src={logo} alt="logo image" className="logo"/>
             <div className="btns">
             {props.isMoviePage && <Button variant="contained" color="primary">Book Show</Button>}
-            {isLoggedIn ? <Button variant="contained" color="default">Login</Button> : <Button variant="contained" color="default">Logout</Button>}
+            {isLoggedIn ? <Button variant="contained" color="default">Logout</Button> : <Button variant="contained" color="default" onClick={loginClickHandler}>Login</Button>}
             </div>
+            <Modal
+                ariaHideApp={false}
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                style={customStyles}
+                contentLabel="Login"
+            >
+                <AppBar position="static" color="default">
+                  <Tabs value={value} 
+                    onChange={handleChange} 
+                    indicatorColor="primary"
+                    textColor="primary"
+                  >
+                    <Tab label="Login" />
+                    <Tab label="Register" />
+                  </Tabs>
+                </AppBar>
+                {value === 0 && <TabContainer>
+                  <Grid
+                    container
+                    direction="column"
+                    justify="center"
+                    alignItems="center"
+                  >
+                    <FormControl required >
+                        <InputLabel htmlFor="username">Username</InputLabel>
+                        <Input id="username" type="text" name="username" onChange={handleLoginFormInputChange} required/>
+                    </FormControl>
+                    <br/><br/>
+                    <FormControl required>
+                        <InputLabel htmlFor="loginPassword">Password</InputLabel>
+                        <Input id="loginPassword" type="password" name="password"  onChange={handleLoginFormInputChange} required/>
+                    </FormControl>
+                    <br/><br/>
+                    {isLoggedIn === true &&
+                        <FormControl>
+                            <span className="successText">
+                                Login Successful!
+                            </span>
+                        </FormControl>
+                    }
+                    <Button variant="contained" color="primary" onClick={loginBtnHandler} >LOGIN</Button>
+                  </Grid>
+                            
+                  </TabContainer>}
+                {value === 1 && <TabContainer>
+                  <Grid
+                    container
+                    direction="column"
+                    justify="center"
+                    alignItems="center"
+                  >  
+                    <FormControl required >
+                          <InputLabel htmlFor="firstName">First Name</InputLabel>
+                          <Input id="firstName" type="text" name="firstName" onChange={handleSignInFormInputChange} required
+                          error={sigInFormInputs.firstName.length > 0 ? false : true}
+                          />
+                      </FormControl>
+                      <br/><br/>
+                      <FormControl required >
+                          <InputLabel htmlFor="lastName">Last Name</InputLabel>
+                          <Input id="lastName" type="text" name="lastName" onChange={handleSignInFormInputChange} required
+                          error={sigInFormInputs.lastName.length > 0 ? false : true}
+                          />
+                      </FormControl>
+                      <br/><br/>
+                      <FormControl required >
+                          <InputLabel htmlFor="email">Email</InputLabel>
+                          <Input id="email" type="email" name="email" onChange={handleSignInFormInputChange} required
+                            error={sigInFormInputs.email.length > 0 ? false : true}
+                          />
+                      </FormControl>
+                      <br/><br/>
+                      <FormControl required>
+                          <InputLabel htmlFor="password">Password</InputLabel>
+                          <Input id="password" type="password" name="password"  onChange={handleSignInFormInputChange} required
+                            error={sigInFormInputs.password.length > 0 ? false : true}
+                          />
+                      </FormControl>   
+                      <br/><br/>               
+                      <FormControl required >
+                          <InputLabel htmlFor="contact">Contact No.</InputLabel>
+                          <Input id="contact" type="text" name="contact" onChange={handleSignInFormInputChange} required
+                            error={sigInFormInputs.contact.length > 0 ? false : true}
+                          />
+                      </FormControl>
+                      <br/><br/>
+                      {isRegistrationSuccessful === true &&
+                          <FormControl>
+                              <span className="successText">
+                              Registration Successful. Please Login!
+                              </span>
+                          </FormControl>
+                      }
+                      <Button variant="contained" color="primary" onClick={signInBtnHandler}>Register</Button>                  
+                    </Grid>
+                  </TabContainer>}
+            </Modal>
         </header>
     )
 }
